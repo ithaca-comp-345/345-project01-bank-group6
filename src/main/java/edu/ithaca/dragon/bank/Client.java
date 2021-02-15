fd001d74a56469a79b7ed6eea0f07f3716a82500
hoshamania <57670943+hoshamania@users.noreply.github.com>
2021-02-14 20:22:00 -0500

    Made edits to client and teller

--- src/main/java/edu/ithaca/dragon/bank/Client.java
+++ src/main/java/edu/ithaca/dragon/bank/Client.java
@@ -1,6 +1,9 @@
 public class Client{
     private BankAccount Checking;
     private BankAccount Savings;
+    private String username;
+    private String password;
+    private String email;
 
     public Client(){
         Checking = null;
@@ -15,6 +18,18 @@ public class Client{
         Savings = s;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public BankAccount getChecking(){
         return Checking;
     }
@@ -23,4 +38,15 @@ public class Client{
         return Savings;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }

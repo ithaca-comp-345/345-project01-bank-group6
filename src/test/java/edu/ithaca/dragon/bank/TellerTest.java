7a7467b0d60be9e77241f62da7933d56839dde7c
hoshamania <57670943+hoshamania@users.noreply.github.com>
2021-02-25 09:11:29 -0500

    Made TellerTest

---
+++ src/test/java/edu/ithaca/dragon/bank/TellerTest.java
@@ -0,0 +1,19 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TellerTest{
+
+    @Test
+    void createAccountTest(){
+
+    }
+
+    @Test
+    void closeAccount(){
+
+    }
+
+}
